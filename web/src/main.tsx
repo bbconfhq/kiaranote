c3aac125841e26efe53f0e81c340117702695a6c
changhui lee <iam@xo.dev>
2023-01-07 16:55:51 +0900

    feat: add router

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,11 +1,30 @@
+import {
+  RouterProvider,
+  createReactRouter,
+  createRouteConfig,
+} from '@tanstack/react-router';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import './index.css';
 
+const rootRoute = createRouteConfig();
+const indexRoute = rootRoute.createRoute({
+  path: '/',
+  component: App,
+});
+const routeConfig = rootRoute.addChildren([indexRoute]);
+const router = createReactRouter({ routeConfig });
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
+
+declare module '@tanstack/react-router' {
+  interface RegisterRouter {
+    router: typeof router
+  }
+}

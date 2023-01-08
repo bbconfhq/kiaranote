906de763616e36370d1f8bf0856b525713d26370
Ryo <9062624+gwanryo@users.noreply.github.com>
2023-01-08 16:27:41 +0900

    refac: PostUsers to PostUser

--- server/tests/internal/handler/user_test.go
+++ server/tests/internal/handler/user_test.go
@@ -85,7 +85,7 @@ func TestV1PostUsers(t *testing.T) {
 	createAdmin(repo, adminUsername, adminPassword)
 	c := loginAdmin(e, adminUsername, adminPassword)
 
-	response := handler.V1PostUsers(&handler.PostUsersRequest{Username: "2", Password: "2"}, c)
+	response := handler.V1PostUser(&handler.PostUsersRequest{Username: "2", Password: "2"}, c)
 	assert.Equal(t, http.StatusCreated, response.Code)
 
 	response = handler.V1GetUsers(nil, c)
@@ -96,7 +96,7 @@ func TestV1PostUsers(t *testing.T) {
 	assert.Equal(t, "1", users[0].Username)
 	assert.Equal(t, "2", users[1].Username)
 
-	response = handler.V1PostUsers(&handler.PostUsersRequest{Username: "2", Password: "2"}, c)
+	response = handler.V1PostUser(&handler.PostUsersRequest{Username: "2", Password: "2"}, c)
 	assert.Equal(t, http.StatusBadRequest, response.Code)
 	assert.Equal(t, constant.ErrBadRequest, response.Error)
 }

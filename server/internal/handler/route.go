d9f3b03f425648f8128c7bd7c5bf35ca82c0c13d
Ryo <9062624+gwanryo@users.noreply.github.com>
2023-01-08 18:13:11 +0900

    add: Note feature

--- server/internal/handler/route.go
+++ server/internal/handler/route.go
@@ -21,4 +21,10 @@ func InitV1Handler(e *echo.Echo) {
 	e.GET("v1/user/:user_id", BaseHandler(GetUserRequest{}, V1GetUser), middleware.AuthMiddleware(constant.RoleUser))
 	e.PUT("v1/user/:user_id", BaseHandler(PutUserRequest{}, V1PutUser), middleware.AuthMiddleware(constant.RoleUser))
 	e.DELETE("v1/user/:user_id", BaseHandler(DeleteUserRequest{}, V1DeleteUser), middleware.AuthMiddleware(constant.RoleUser))
+
+	e.GET("v1/note", BaseHandler(GetNotesRequest{}, V1GetNotes), middleware.AuthMiddleware(constant.RoleUser))
+	e.POST("v1/note", BaseHandler(PostNoteRequest{}, V1PostNote), middleware.AuthMiddleware(constant.RoleAdmin))
+	e.GET("v1/note/:note_id", BaseHandler(GetNoteRequest{}, V1GetNote), middleware.AuthMiddleware(constant.RoleUser))
+	e.PUT("v1/note/:note_id", BaseHandler(PutNoteRequest{}, V1PutNote), middleware.AuthMiddleware(constant.RoleAdmin))
+	e.DELETE("v1/note/:note_id", BaseHandler(DeleteNoteRequest{}, V1DeleteNote), middleware.AuthMiddleware(constant.RoleAdmin))
 }

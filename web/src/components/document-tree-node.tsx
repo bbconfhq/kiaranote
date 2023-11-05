99ac1ce1996d2d785d7597f7c60075695c5ad1eb
witch-factory <soakdma37@gmail.com>
2023-11-06 00:28:37 +0900

    @radix-ui/react-context-menu 설치, ContextMenuWrapper 제작

--- web/src/components/document-tree-node.tsx
+++ web/src/components/document-tree-node.tsx
@@ -1,4 +1,5 @@
 import { NodeModel } from '@minoru/react-dnd-treeview';
+import * as ContextMenu from '@radix-ui/react-context-menu';
 import { FileTextIcon } from '@radix-ui/react-icons';
 import React, { MouseEventHandler } from 'react';
 
@@ -58,6 +59,26 @@ const Label = styled('div', {
   }
 });
 
+
+const ContextMenuWrapper=({children}: {children: React.ReactNode})=>{
+  return (
+    <ContextMenu.Root>
+      <ContextMenu.Trigger>
+        {children}
+      </ContextMenu.Trigger>
+      <ContextMenu.Portal>
+        <ContextMenu.Content>
+          <ContextMenu.Group>
+            <ContextMenu.Item onSelect={() => alert('Cut')}>Cut</ContextMenu.Item>
+            <ContextMenu.Item onSelect={() => alert('Copy')}>Copy</ContextMenu.Item>
+            <ContextMenu.Item onSelect={() => alert('Paste')}>Paste</ContextMenu.Item>
+          </ContextMenu.Group>
+        </ContextMenu.Content>
+      </ContextMenu.Portal>
+    </ContextMenu.Root>
+  );
+};
+
 const Node = ({node, depth, isOpen, isLeaf, onClick }: {
   node: NodeModel;
   depth: number;
@@ -73,26 +94,29 @@ const Node = ({node, depth, isOpen, isLeaf, onClick }: {
   };
 
   return (
-    <Wrapper style={{ marginInlineStart: indent }} onClick={handleToggle}>
-      <ExpandIconWrapper expanded={isOpen}>
-        {!isLeaf && node.droppable && (
-          <svg
-            width='16'
-            height='16'
-            viewBox='0 0 16 16'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <path
-              d='M10.5866 5.99969L7.99997 8.58632L5.41332 5.99969C5.15332 5.73969 4.73332 5.73969 4.47332 5.99969C4.21332 6.25969 4.21332 6.67965 4.47332 6.93965L7.5333 9.99965C7.59497 10.0615 7.66823 10.1105 7.7489 10.144C7.82957 10.1775 7.91603 10.1947 8.0033 10.1947C8.09063 10.1947 8.1771 10.1775 8.25777 10.144C8.33837 10.1105 8.41163 10.0615 8.4733 9.99965L11.5333 6.93965C11.7933 6.67965 11.7933 6.25969 11.5333 5.99969C11.2733 5.74635 10.8466 5.73969 10.5866 5.99969Z'
-              fill='black'
-            />
-          </svg>
-        )}
-      </ExpandIconWrapper>
-      <FileTextIcon width={16} height={16} />
-      <Label>{node.text}</Label>
-    </Wrapper>
+    <ContextMenuWrapper>
+      <Wrapper style={{ marginInlineStart: indent }} onClick={handleToggle}>
+        <ExpandIconWrapper expanded={isOpen}>
+          {!isLeaf && node.droppable && (
+            <svg
+              width='16'
+              height='16'
+              viewBox='0 0 16 16'
+              fill='none'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                d='M10.5866 5.99969L7.99997 8.58632L5.41332 5.99969C5.15332 5.73969 4.73332 5.73969 4.47332 5.99969C4.21332 6.25969 4.21332 6.67965 4.47332 6.93965L7.5333 9.99965C7.59497 10.0615 7.66823 10.1105 7.7489 10.144C7.82957 10.1775 7.91603 10.1947 8.0033 10.1947C8.09063 10.1947 8.1771 10.1775 8.25777 10.144C8.33837 10.1105 8.41163 10.0615 8.4733 9.99965L11.5333 6.93965C11.7933 6.67965 11.7933 6.25969 11.5333 5.99969C11.2733 5.74635 10.8466 5.73969 10.5866 5.99969Z'
+                fill='black'
+              />
+            </svg>
+          )}
+        </ExpandIconWrapper>
+        <FileTextIcon />
+        <Label>{node.text}</Label>
+      </Wrapper>
+    </ContextMenuWrapper>
+
   );
 };
 
